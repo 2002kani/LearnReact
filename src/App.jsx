8898eeb95c3160dae5e5aba19a0ea6afe4bc7237
02Kani <kanikaradag02@gmail.com>
2024-12-20 21:14:42 +0100

    added Star Ratings

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Accordian from "./Components/Accordian/index.jsx";
 import RandomColor from "./Components/Color-generator/index.jsx";
+import StarRating from "./Components/Star-rating/index.jsx";
 
 function App() {
   return(
     <div className="App">
-        {/*<Accordian/>*/}
-        <RandomColor/>
+        {/* <Accordian/> */}
+        {/* <RandomColor/> */}
+        <StarRating />
     </div>
   );
 }
